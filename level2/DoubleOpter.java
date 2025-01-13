8b8e1a38e6a1d00507b32346019dd6a073aebd1d
heynexus007 <abthegamer007@gmail.com>
2025-01-13 11:52:49 +0530

    Double Operator Prob3

---
+++ level2/DoubleOpter.java
@@ -0,0 +1,20 @@
+//3
+import java.util.Scanner;
+class DoubleOpter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Enter value of a, b and c
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double c = input.nextDouble();
+
+        double result1 = a + b * c;
+        double result2 = a * b + c;
+        double result3 = c + a / b;
+        double result4 = a % b + c;
+
+        System.out.println("The results of Double Operations are " + result1 + ", " + result2 + ", " + result3 + ", and " + result4);
+		
+		input.close();
+    }
+}
